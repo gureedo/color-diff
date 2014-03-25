60b40a0e63ecd27d300d6d2f24a19c19fac1af3d
gureedo <gureedo@intersvyaz.net>
2014-03-25 09:19:49 +0600

    initial commit

---
+++ src/common/main.js
@@ -0,0 +1,17 @@
+﻿// ==UserScript==
+// @name		color-diff
+// @include		*.diff
+// ==/UserScript==
+
+(function(){
+	"use strict";
+
+	var textElem = document.querySelector('pre');
+	var text = textElem.innerHTML;
+	textElem.innerHTML = text
+		.replace(/^\+.*$/gm, '<span style="font-weight: bold; color: green;">$&</span>')
+		.replace(/^-.*$/gm, '<span style="font-weight: bold; color: red;">$&</span>')
+		.replace(/^@.*$/gm, '<span style="font-weight: bold; color: blue;">$&</span>')
+		.replace(/^([iI]ndex:?|diff --git) .*$/gim,
+			'<span style="font-weight: bold;">$&</span>');
+})();
